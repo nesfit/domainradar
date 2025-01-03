6d22573201b29eb1631f92083073fc33f60ee575
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2025-01-03 14:26:31 +0100

    Return to proper working directory

--- build_images.sh
+++ build_images.sh
@@ -2,15 +2,17 @@
 
 source options.sh
 
+working_dir="$PWD"
+
 echo "Building the domrad/loader image"
 $DOCKER build -t domrad/loader -f- "$LOADER_DIR" < dockerfiles/prefilter.Dockerfile
 
 echo "Building the domrad/webui image"
 cd "$WEBUI_DIR" || exit 1
 $DOCKER build -t domrad/webui .
-cd ..
+cd "$working_dir"
 
 echo "Building the pipeline images"
 cd "$COLEXT_DIR" || exit 1
 ./build_images.sh
-cd ..
+cd "$working_dir"
